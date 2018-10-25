2325a644e7653a76e329aaacd1e96735bd1261fd
Idan Morad <idan.morad@cyberbit.com>
2018-10-25 10:26:44 +0300

    add feature visualization

--- ds_utils/visualization_aids.py
+++ ds_utils/visualization_aids.py
@@ -1,8 +1,11 @@
 from typing import Optional, List
 
+import pandas
 import pydotplus
+import seaborn
 import sklearn
 from IPython.display import Image
+from matplotlib import pyplot
 from sklearn.externals.six import StringIO
 from sklearn.tree import export_graphviz
 
@@ -19,3 +22,30 @@ def draw_tree(tree: sklearn.tree.tree.BaseDecisionTree, features_names: Optional
                     special_characters=True)
     graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
     return Image(graph.create_png())
+
+
+def visualize_features(frame: pandas.DataFrame) -> None:
+    """
+    Visualize features values on graphs.
+    :param frame: the data frame.
+    """
+    features = frame.columns
+    figure, axes = pyplot.subplots(nrows=int(len(features) / 2) + 1, ncols=2, figsize=(20, 30))
+    axes = axes.flatten()
+    i = 0
+
+    for feature in features:
+        if frame[feature].dtype == "float64":
+            plot = seaborn.distplot(frame[feature], ax=axes[i])
+        else:
+            plot = seaborn.countplot(frame[feature], ax=axes[i])
+        plot.set_title(f"{feature} ({frame[feature].dtype})")
+        plot.set_xlabel("")
+
+        pyplot.setp(axes[i].get_xticklabels(), rotation=45)
+        i += 1
+
+    if len(features) % 2 == 1:
+        figure.delaxes(axes[i])
+    pyplot.subplots_adjust(hspace=0.5)
+    pyplot.show()
