107d398512274d056db9b942434a65878d0b9e43
Idan Morad <idan.morad@cyberbitlabs.com>
2019-12-09 08:59:46 +0200

    ### Added - visualization_aids::print_decision_paths a method that converts decision tree to a python function. ### Changed - visualization_aids::draw_tree parameter features_names changed to feature_names. - visualization_aids::draw_tree parameters feature_names and class_names received default value. ### Fixed - Changed import of sklearn.tree.tree to sklearn.tree due to FutureWarning.

--- tests/test_visualization_aids.py
+++ tests/test_visualization_aids.py
@@ -1,10 +1,11 @@
+import os
 from pathlib import Path
 
 import pandas
 from sklearn import datasets
 from sklearn.tree import DecisionTreeClassifier
 
-from ds_utils.visualization_aids import draw_tree, visualize_features
+from ds_utils.visualization_aids import draw_tree, visualize_features, print_decision_paths
 from tests.utils import compare_images_paths
 
 
@@ -49,3 +50,147 @@ def test_visualize_features():
     baseline_path = Path(__file__).parents[0].absolute().joinpath("baseline_images").joinpath(
         "test_visualization_aids").joinpath("test_visualize_features.png")
     compare_images_paths(str(baseline_path), str(result_path))
+
+
+def test_print_decision_paths():
+    iris = datasets.load_iris()
+    x = iris.data
+    y = iris.target
+
+    # Create decision tree classifier object
+    clf = DecisionTreeClassifier(random_state=0, max_depth=3)
+
+    # Train model
+    clf.fit(x, y)
+
+    result = print_decision_paths(clf, iris.feature_names, iris.target_names.tolist(), "iris_tree")
+
+    expected = 'def iris_tree(petal width (cm), petal length (cm)):' + os.linesep + \
+               '  if petal width (cm) <= 0.8000:' + os.linesep + \
+               '    # return class setosa with probability 0.9804' + os.linesep + \
+               '    return ("setosa", 0.9804)' + os.linesep + \
+               '  else:  # if petal width (cm) > 0.8000' + os.linesep + \
+               '    if petal width (cm) <= 1.7500:' + os.linesep + \
+               '      if petal length (cm) <= 4.9500:' + os.linesep + \
+               '        # return class versicolor with probability 0.9792' + os.linesep + \
+               '        return ("versicolor", 0.9792)' + os.linesep + \
+               '      else:  # if petal length (cm) > 4.9500' + os.linesep + \
+               '        # return class virginica with probability 0.6667' + os.linesep + \
+               '        return ("virginica", 0.6667)' + os.linesep + \
+               '    else:  # if petal width (cm) > 1.7500' + os.linesep + \
+               '      if petal length (cm) <= 4.8500:' + os.linesep + \
+               '        # return class virginica with probability 0.6667' + os.linesep + \
+               '        return ("virginica", 0.6667)' + os.linesep + \
+               '      else:  # if petal length (cm) > 4.8500' + os.linesep + \
+               '        # return class virginica with probability 0.9773' + os.linesep + \
+               '        return ("virginica", 0.9773)' + os.linesep
+
+    assert result == expected
+
+
+def test_print_decision_paths_no_tree_name():
+    iris = datasets.load_iris()
+    x = iris.data
+    y = iris.target
+
+    # Create decision tree classifier object
+    clf = DecisionTreeClassifier(random_state=0, max_depth=3)
+
+    # Train model
+    clf.fit(x, y)
+
+    result = print_decision_paths(clf, iris.feature_names, iris.target_names.tolist())
+
+    expected = 'def tree(petal width (cm), petal length (cm)):' + os.linesep + \
+               '  if petal width (cm) <= 0.8000:' + os.linesep + \
+               '    # return class setosa with probability 0.9804' + os.linesep + \
+               '    return ("setosa", 0.9804)' + os.linesep + \
+               '  else:  # if petal width (cm) > 0.8000' + os.linesep + \
+               '    if petal width (cm) <= 1.7500:' + os.linesep + \
+               '      if petal length (cm) <= 4.9500:' + os.linesep + \
+               '        # return class versicolor with probability 0.9792' + os.linesep + \
+               '        return ("versicolor", 0.9792)' + os.linesep + \
+               '      else:  # if petal length (cm) > 4.9500' + os.linesep + \
+               '        # return class virginica with probability 0.6667' + os.linesep + \
+               '        return ("virginica", 0.6667)' + os.linesep + \
+               '    else:  # if petal width (cm) > 1.7500' + os.linesep + \
+               '      if petal length (cm) <= 4.8500:' + os.linesep + \
+               '        # return class virginica with probability 0.6667' + os.linesep + \
+               '        return ("virginica", 0.6667)' + os.linesep + \
+               '      else:  # if petal length (cm) > 4.8500' + os.linesep + \
+               '        # return class virginica with probability 0.9773' + os.linesep + \
+               '        return ("virginica", 0.9773)' + os.linesep
+
+    assert result == expected
+
+
+def test_print_decision_paths_no_feature_names():
+    iris = datasets.load_iris()
+    x = iris.data
+    y = iris.target
+
+    # Create decision tree classifier object
+    clf = DecisionTreeClassifier(random_state=0, max_depth=3)
+
+    # Train model
+    clf.fit(x, y)
+
+    result = print_decision_paths(clf, None, iris.target_names.tolist(), "iris_tree")
+
+    expected = 'def iris_tree(feature_3, feature_2):' + os.linesep + \
+               '  if feature_3 <= 0.8000:' + os.linesep + \
+               '    # return class setosa with probability 0.9804' + os.linesep + \
+               '    return ("setosa", 0.9804)' + os.linesep + \
+               '  else:  # if feature_3 > 0.8000' + os.linesep + \
+               '    if feature_3 <= 1.7500:' + os.linesep + \
+               '      if feature_2 <= 4.9500:' + os.linesep + \
+               '        # return class versicolor with probability 0.9792' + os.linesep + \
+               '        return ("versicolor", 0.9792)' + os.linesep + \
+               '      else:  # if feature_2 > 4.9500' + os.linesep + \
+               '        # return class virginica with probability 0.6667' + os.linesep + \
+               '        return ("virginica", 0.6667)' + os.linesep + \
+               '    else:  # if feature_3 > 1.7500' + os.linesep + \
+               '      if feature_2 <= 4.8500:' + os.linesep + \
+               '        # return class virginica with probability 0.6667' + os.linesep + \
+               '        return ("virginica", 0.6667)' + os.linesep + \
+               '      else:  # if feature_2 > 4.8500' + os.linesep + \
+               '        # return class virginica with probability 0.9773' + os.linesep + \
+               '        return ("virginica", 0.9773)' + os.linesep
+
+    assert result == expected
+
+
+def test_print_decision_paths_no_class_names():
+    iris = datasets.load_iris()
+    x = iris.data
+    y = iris.target
+
+    # Create decision tree classifier object
+    clf = DecisionTreeClassifier(random_state=0, max_depth=3)
+
+    # Train model
+    clf.fit(x, y)
+
+    result = print_decision_paths(clf, iris.feature_names, None, "iris_tree")
+
+    expected = 'def iris_tree(petal width (cm), petal length (cm)):' + os.linesep + \
+               '  if petal width (cm) <= 0.8000:' + os.linesep + \
+               '    # return class class_0 with probability 0.9804' + os.linesep + \
+               '    return ("class_0", 0.9804)' + os.linesep + \
+               '  else:  # if petal width (cm) > 0.8000' + os.linesep + \
+               '    if petal width (cm) <= 1.7500:' + os.linesep + \
+               '      if petal length (cm) <= 4.9500:' + os.linesep + \
+               '        # return class class_1 with probability 0.9792' + os.linesep + \
+               '        return ("class_1", 0.9792)' + os.linesep + \
+               '      else:  # if petal length (cm) > 4.9500' + os.linesep + \
+               '        # return class class_2 with probability 0.6667' + os.linesep + \
+               '        return ("class_2", 0.6667)' + os.linesep + \
+               '    else:  # if petal width (cm) > 1.7500' + os.linesep + \
+               '      if petal length (cm) <= 4.8500:' + os.linesep + \
+               '        # return class class_2 with probability 0.6667' + os.linesep + \
+               '        return ("class_2", 0.6667)' + os.linesep + \
+               '      else:  # if petal length (cm) > 4.8500' + os.linesep + \
+               '        # return class class_2 with probability 0.9773' + os.linesep + \
+               '        return ("class_2", 0.9773)' + os.linesep
+
+    assert result == expected
