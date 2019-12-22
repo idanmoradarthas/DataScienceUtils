91e1ca2624378e547abb9d2fcb2ea8be095841ea
Idan Morad <idan.morad@cyberbitlabs.com>
2019-12-22 09:30:52 +0200

    * fix tests

--- tests/utils.py
+++ tests/utils.py
@@ -1,3 +1,5 @@
+import sys
+
 import matplotlib
 
 matplotlib.use('agg')
@@ -6,5 +8,6 @@ from matplotlib.testing.compare import compare_images
 
 def compare_images_paths(first: str, second: str) -> None:
     results = compare_images(first, second, 10)
+    sys.stderr.write(results)
     if results is not None:  # the images compare favorably
         assert False
