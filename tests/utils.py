c155e952dcccd7a0c5972644856123ea39504150
Idan Morad <idan.morad@cyberbitlabs.com>
2019-12-22 09:40:27 +0200

    * add services to travis

--- tests/utils.py
+++ tests/utils.py
@@ -1,13 +1,7 @@
-import sys
-
-import matplotlib
-
-matplotlib.use('agg')
 from matplotlib.testing.compare import compare_images
 
 
 def compare_images_paths(first: str, second: str) -> None:
     results = compare_images(first, second, 10)
-    sys.stderr.write(results)
     if results is not None:  # the images compare favorably
         assert False
