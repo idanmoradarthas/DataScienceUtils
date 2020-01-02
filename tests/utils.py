3cafb27923c4b7a02183f55fd741dc7247bdd485
Idan Morad <idan.morad@cyberbitlabs.com>
2020-01-02 13:32:52 +0200

    fix tests

--- tests/utils.py
+++ tests/utils.py
@@ -1,12 +1,9 @@
-import sys
-
 from matplotlib.testing.compare import compare_images
 
-TOLERANCE = 40
+TOLERANCE = 50
 
 
 def compare_images_paths(first: str, second: str) -> None:
     results = compare_images(first, second, TOLERANCE)
     if results is not None:  # the images compare favorably
-        sys.stderr.write(results)
         assert False
