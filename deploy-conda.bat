9e0188cb270daccc4ca6f3922b6dac7ceba8d03b
Idan Morad <idan.morad@elbitsystems.com>
2020-07-09 07:51:29 +0300

    fix conda deploy

--- deploy-conda.bat
+++ deploy-conda.bat
@@ -10,18 +10,18 @@ conda build purge
 
 :: copy directory win-64 from C:\Anaconda3\envs\DataScienceUtils\conda-bld to outputdir directory
 
-conda convert -f --platform all outputdir\win-64\data-science-utils-1.6.2-py36_0.tar.bz2 -o outputdir\
-conda convert -f --platform all outputdir\win-64\data-science-utils-1.6.2-py37_0.tar.bz2 -o outputdir\
-conda convert -f --platform all outputdir\win-64\data-science-utils-1.6.2-py38_0.tar.bz2 -o outputdir\
+conda convert -f --platform all outputdir\win-64\data-science-utils-1.6.3-py36_0.tar.bz2 -o outputdir\
+conda convert -f --platform all outputdir\win-64\data-science-utils-1.6.3-py37_0.tar.bz2 -o outputdir\
+conda convert -f --platform all outputdir\win-64\data-science-utils-1.6.3-py38_0.tar.bz2 -o outputdir\
 
 anaconda login
 
-anaconda upload outputdir/linux-32/data-science-utils-1.6.2-py36_0.tar.bz2 outputdir/linux-32/data-science-utils-1.6.2-py37_0.tar.bz2 outputdir/linux-32/data-science-utils-1.6.2-py38_0.tar.bz2
-anaconda upload outputdir/linux-64/data-science-utils-1.6.2-py36_0.tar.bz2 outputdir/linux-64/data-science-utils-1.6.2-py37_0.tar.bz2 outputdir/linux-64/data-science-utils-1.6.2-py38_0.tar.bz2
-anaconda upload outputdir/linux-aarch64/data-science-utils-1.6.2-py36_0.tar.bz2 outputdir/linux-aarch64/data-science-utils-1.6.2-py37_0.tar.bz2 outputdir/linux-aarch64/data-science-utils-1.6.2-py38_0.tar.bz2
-anaconda upload outputdir/linux-armv6l/data-science-utils-1.6.2-py36_0.tar.bz2 outputdir/linux-armv6l/data-science-utils-1.6.2-py37_0.tar.bz2 outputdir/linux-armv6l/data-science-utils-1.6.2-py38_0.tar.bz2
-anaconda upload outputdir/linux-armv7l/data-science-utils-1.6.2-py36_0.tar.bz2 outputdir/linux-armv7l/data-science-utils-1.6.2-py37_0.tar.bz2 outputdir/linux-armv7l/data-science-utils-1.6.2-py38_0.tar.bz2
-anaconda upload outputdir/linux-ppc64le/data-science-utils-1.6.2-py36_0.tar.bz2 outputdir/linux-ppc64le/data-science-utils-1.6.2-py37_0.tar.bz2 outputdir/linux-ppc64le/data-science-utils-1.6.2-py38_0.tar.bz2
-anaconda upload outputdir/osx-64/data-science-utils-1.6.2-py36_0.tar.bz2 outputdir/osx-64/data-science-utils-1.6.2-py37_0.tar.bz2 outputdir/osx-64/data-science-utils-1.6.2-py38_0.tar.bz2
-anaconda upload outputdir/win-32/data-science-utils-1.6.2-py36_0.tar.bz2 outputdir/win-32/data-science-utils-1.6.2-py37_0.tar.bz2 outputdir/win-32/data-science-utils-1.6.2-py38_0.tar.bz2
-anaconda upload outputdir/win-64/data-science-utils-1.6.2-py36_0.tar.bz2 outputdir/win-64/data-science-utils-1.6.2-py37_0.tar.bz2 outputdir/win-64/data-science-utils-1.6.2-py38_0.tar.bz2
+anaconda upload outputdir/linux-32/data-science-utils-1.6.3-py36_0.tar.bz2 outputdir/linux-32/data-science-utils-1.6.3-py37_0.tar.bz2 outputdir/linux-32/data-science-utils-1.6.3-py38_0.tar.bz2
+anaconda upload outputdir/linux-64/data-science-utils-1.6.3-py36_0.tar.bz2 outputdir/linux-64/data-science-utils-1.6.3-py37_0.tar.bz2 outputdir/linux-64/data-science-utils-1.6.3-py38_0.tar.bz2
+anaconda upload outputdir/linux-aarch64/data-science-utils-1.6.3-py36_0.tar.bz2 outputdir/linux-aarch64/data-science-utils-1.6.3-py37_0.tar.bz2 outputdir/linux-aarch64/data-science-utils-1.6.3-py38_0.tar.bz2
+anaconda upload outputdir/linux-armv6l/data-science-utils-1.6.3-py36_0.tar.bz2 outputdir/linux-armv6l/data-science-utils-1.6.3-py37_0.tar.bz2 outputdir/linux-armv6l/data-science-utils-1.6.3-py38_0.tar.bz2
+anaconda upload outputdir/linux-armv7l/data-science-utils-1.6.3-py36_0.tar.bz2 outputdir/linux-armv7l/data-science-utils-1.6.3-py37_0.tar.bz2 outputdir/linux-armv7l/data-science-utils-1.6.3-py38_0.tar.bz2
+anaconda upload outputdir/linux-ppc64le/data-science-utils-1.6.3-py36_0.tar.bz2 outputdir/linux-ppc64le/data-science-utils-1.6.3-py37_0.tar.bz2 outputdir/linux-ppc64le/data-science-utils-1.6.3-py38_0.tar.bz2
+anaconda upload outputdir/osx-64/data-science-utils-1.6.3-py36_0.tar.bz2 outputdir/osx-64/data-science-utils-1.6.3-py37_0.tar.bz2 outputdir/osx-64/data-science-utils-1.6.3-py38_0.tar.bz2
+anaconda upload outputdir/win-32/data-science-utils-1.6.3-py36_0.tar.bz2 outputdir/win-32/data-science-utils-1.6.3-py37_0.tar.bz2 outputdir/win-32/data-science-utils-1.6.3-py38_0.tar.bz2
+anaconda upload outputdir/win-64/data-science-utils-1.6.3-py36_0.tar.bz2 outputdir/win-64/data-science-utils-1.6.3-py37_0.tar.bz2 outputdir/win-64/data-science-utils-1.6.3-py38_0.tar.bz2
