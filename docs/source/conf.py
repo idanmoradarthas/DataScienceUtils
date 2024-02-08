845cdbd763e6df5f35269007dfb2fe87e3bcc7f0
idan.morad <idan.morad@rsa.com>
2024-02-08 20:08:01 +0200

    minor change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,8 +7,6 @@ import datetime
 import os
 import sys
 
-import ds_utils
-
 # -- Path setup --------------------------------------------------------------
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -16,6 +14,7 @@ import ds_utils
 
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath("../../ds_utils"))
+import ds_utils
 
 # -- Project information -----------------------------------------------------
 
