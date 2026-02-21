37a43af56da5d9db2fff81f70ef422ba171687b8
Idan Morad <idanmorad.arthas@gmail.com>
2026-02-21 21:24:25 +0200

    Move baseline images and tests for visualization functions
    - Move new baseline images for the plot_correlation_dendrogram, plot_features_interaction, and visualize_correlations functions to ensure consistent visual outputs.
    - Split tests/test_preprocess/test_visualization.py to test_visualize_feature.py, test_visualize_correlations.py, test_plot_correlation_dendrogram.py, test_plot_relationship_between_features.py, test_plot_features_interaction.py

---
+++ tests/test_preprocess/test_visualize_correlations.py
@@ -0,0 +1,28 @@
+"""Testing visualize_correlations function."""
+
+from pathlib import Path
+
+from matplotlib import pyplot as plt
+import pytest
+
+from ds_utils.preprocess.visualization import visualize_correlations
+
+BASELINE_DIR = Path(__file__).parent.parent / "baseline_images" / "test_preprocess" / "test_visualize_correlations"
+
+
+@pytest.mark.mpl_image_compare(baseline_dir=BASELINE_DIR)
+@pytest.mark.parametrize("use_existing_ax", [False, True], ids=["default", "exist_ax"])
+def test_visualize_correlations(data_1m, use_existing_ax):
+    """Test visualize_correlations function with and without existing axes."""
+    corr = data_1m.apply(lambda x: x.factorize()[0]).corr(method="pearson", min_periods=1)
+    if use_existing_ax:
+        _, ax = plt.subplots()
+        ax.set_title("My ax")
+        visualize_correlations(corr, ax=ax)
+        assert ax.get_title() == "My ax"
+    else:
+        visualize_correlations(corr)
+
+    fig = plt.gcf()
+    fig.set_size_inches(14, 9)
+    return fig
